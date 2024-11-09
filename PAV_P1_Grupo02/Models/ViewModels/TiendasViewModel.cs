71bb62e1a80ab324b1b943ee9134991383e7a09a
Veronica Bustos <verobg16@gmail.com>
2024-11-08 18:51:06 -0600

    TiendasViewModel Completado

--- PAV_P1_Grupo02/Models/ViewModels/TiendasViewModel.cs
+++ PAV_P1_Grupo02/Models/ViewModels/TiendasViewModel.cs
@@ -7,5 +7,18 @@ namespace PAV_P1_Grupo02.Models.ViewModels
 {
     public class TiendasViewModel
     {
+        [Display(Name = "ID")]
+        public int Id_Producto { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Direccion { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        public int Stock { get; set; }
     }
 }
