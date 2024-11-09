00d41d92506ba4d3d7b2e11e0dc54bccc5d218f1
Dennis Mendez <denn.mendez.del@gmail.com>
2024-11-08 18:49:12 -0600

    Creación de controladores, vistas y models del proyecto completo.

---
+++ PAV_P1_Grupo02/Controllers/TiendasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PAV_P1_Grupo02.Controllers
+{
+    public class TiendasController : Controller
+    {
+        // GET: Tiendas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PAV_P1_Grupo02/Models/ViewModels/TiendasViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PAV_P1_Grupo02.Models.ViewModels
+{
+    public class TiendasViewModel
+    {
+    }
+}
