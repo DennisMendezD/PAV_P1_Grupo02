c7327dbd05e6cb3671a5d5b446a7c77126c7d9fe
Dennis Mendez <denn.mendez.del@gmail.com>
2024-11-08 18:10:58 -0600

    Creación de vista, modelo y controlador para Personas.

---
+++ PAV_P1_Grupo02/Models/ViewModels/PersonasViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PAV_P1_Grupo02.Models.ViewModels
+{
+    public class PersonasViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Identificacion { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre completo")]
+        public string NombreCompleto { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+
+        [Required]
+        public string Estado { get; set; }
+
+    }
+}
